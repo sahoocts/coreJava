36e8885d876481834fc5df2e0a6d26aa28fa3cbf
sahoocts <46101188+sahoocts@users.noreply.github.com>
2022-08-05 11:22:06 +0530

    Add files via upload

---
+++ FirstRepeat.java
@@ -0,0 +1,27 @@
+package PrevPractice;
+
+public class FirstRepeat {
+    public static void main(String args[]){
+        String str="aabcd";
+        int fre[]=new int[26];
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            int digit=ch-'a';
+            fre[digit]=fre[digit]+1;
+          //  System.out.print(digit);
+        }
+
+        for(int i=0;i<str.length();i++){
+           // char c=' ';
+            char c1=str.charAt(i);
+            int di=c1-'a';
+           // if(fre[di]==1){//first non repeating char
+            if(fre[di]>=1){//first repeating char
+                System.out.print(str.charAt(i));
+                break;
+            }
+
+        }
+
+    }
+}
